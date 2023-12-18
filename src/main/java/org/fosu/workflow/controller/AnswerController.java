bd6c17fdcec55e4f34d4e80a853bcaa27207e53b
Alexander_GaGa <1695906287@qq.com>
2023-12-18 23:23:57 +0800

    答题模块

--- src/main/java/org/fosu/workflow/controller/AnswerController.java
+++ src/main/java/org/fosu/workflow/controller/AnswerController.java
@@ -4,40 +4,89 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.fosu.workflow.entities.Answer;
 import org.fosu.workflow.entities.ChoiceQuestion;
+import org.fosu.workflow.entities.Paper;
 import org.fosu.workflow.req.AnswerREQ;
 import org.fosu.workflow.req.QuestionREQ;
 import org.fosu.workflow.service.AnswerService;
 import org.fosu.workflow.service.ChoiceQuestionService;
+import org.fosu.workflow.service.PaperService;
+import org.fosu.workflow.service.StudentService;
 import org.fosu.workflow.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@Api("简答题申请控制层")
+import java.util.Date;
+
+@Api("答题申请控制层")
 @ResponseBody
 @RestController
 @RequestMapping("/answer")
 public class AnswerController {
     @Autowired
     AnswerService answerService;
-    @ApiOperation("新增简答题申请")
+    @Autowired
+    ChoiceQuestionService choiceService;
+    @Autowired
+    PaperService paperService;
+    @Autowired
+    StudentService studentService;
+    @ApiOperation("新增答题申请")
     @PostMapping
     public Result add(@RequestBody Answer answer) {
+        if(isTimeEnd(answer))
+            return Result.error("试卷已经截止");
+        UpdateScore(answer);
         return answerService.add(answer);
     }
-    @ApiOperation("查询简答题请列表")
+    @ApiOperation("查询答题请列表")
     @PostMapping("/list")
     public Result listPage(@RequestBody AnswerREQ req) {
         return answerService.listPage(req);
     }
-    @ApiOperation("更新简答题详情信息")
+    @ApiOperation("更新答题详情信息")
     @PutMapping
     public Result update(@RequestBody Answer answer) {
+        if(isTimeEnd(answer))
+            return Result.error("试卷已经截止,学生不可再修改，仅允许老师修改");
+        UpdateScore(answer);
         return answerService.update(answer);
     }
-    @ApiOperation("查询简答题详情信息")
+    @ApiOperation("查询答题详情信息")
     @GetMapping("/{id}")
     public Result view(@PathVariable String id) {
         Answer entity = answerService.getById(id);
         return Result.ok(entity);
+    }
+    void UpdateScore(Answer answer)
+    {
+        if(answer == null)
+            return;
+        int intAnswer = 0;
+        try
+        {
+            intAnswer = Integer.parseInt(answer.getAnswer());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        ChoiceQuestion choiceQuestion = choiceService.getById(answer.getQuestionId());//判断题自动批改
+        if(choiceQuestion != null && intAnswer == choiceQuestion.getAnswer())
+        {
+            answer.setScore(choiceQuestion.getScore());
+        }
+    }
+    boolean isTimeEnd(Answer answer)
+    {
+        if(answer != null)
+        {
+            Paper paper = paperService.getById(answer.getPaperId());
+            if(paper != null)
+            {
+                if(paper.getEndDate() == null)
+                    return false;
+                return new Date().after(paper.getEndDate());
+            }
+        }
+        return true;
     }
 }
